eec33e62437cbbcd5421933361f362303ed4d25e
AdamCCC <fade.20.away@gmail.com>
2016-09-16 23:58:52 +1000

    Update GameView.c

--- GameView.c
+++ GameView.c
@@ -275,125 +275,168 @@ LocationID getLocation(GameView currentView, PlayerID player)
 // Fills the trail array with the location ids of the last 6 turns
 
 void getHistory(GameView currentView, PlayerID player,
+
                             LocationID trail[TRAIL_SIZE])
 
 {
-/*  trail[0]=UNKNOWN_LOCATION;
-    trail[1]=UNKNOWN_LOCATION;
-    trail[2]=UNKNOWN_LOCATION;
-    trail[3]=UNKNOWN_LOCATION;
-    trail[4]=UNKNOWN_LOCATION;
-    trail[5]=UNKNOWN_LOCATION;
+/*	trail[0]=UNKNOWN_LOCATION;
+	trail[1]=UNKNOWN_LOCATION;
+	trail[2]=UNKNOWN_LOCATION;
+	trail[3]=UNKNOWN_LOCATION;
+	trail[4]=UNKNOWN_LOCATION;
+	trail[5]=UNKNOWN_LOCATION;
 */
-    int i = 0;
-    while(i<TRAIL_SIZE){
-        trail[i]=currentView->location[player][i];
-        i++;
-    }
+	int i = 0;
+
+	while(i<TRAIL_SIZE){
+
+		trail[i]=currentView->location[player][i];
+
+		i++;
+
+	}
+
+    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+
 }
+
 
 
 //fills array recording the last 6 location ids of all players
 
 static void lastSix(GameView currentView, char *pastPlays){
+    printf("round be 1 but is %d and %d\n", roundCalc(pastPlays), currentView->currRound);
     //if everyone has taken at least 6 turns
-    //NOT RIGHT
+    printf("%d\n",currentView->currRound);
     if((currentView->currRound)>=6){
         //goes to the 6th last round
-        int index = ((currentView->currRound)-6)*8 + 1;
+        int index = ((currentView->currRound)-6)*SAME_PLACE_NEXT_TURN*NUM_PLAYERS + 1;
         PlayerID i = 0;
-        while(i<NUM_PLAYERS){
-            int turn = 0;
-            while(turn<6){
-                if(pastPlays[index]=='C' && pastPlays[index+1]=='?'){
-                    currentView->location[i][TRAIL_SIZE-1-turn] = CITY_UNKNOWN;
-                } else if(pastPlays[index]=='S' && pastPlays[index+1]=='?'){
-                    currentView->location[i][TRAIL_SIZE-1-turn] = SEA_UNKNOWN;
-                } else if(pastPlays[index]=='H' && pastPlays[index+1]=='I'){
-                    currentView->location[i][TRAIL_SIZE-1-turn] = HIDE;
-                } else if(pastPlays[index]=='D' && pastPlays[index+1]=='1'){
-                    currentView->location[i][TRAIL_SIZE-1-turn] = DOUBLE_BACK_1;
-                } else if(pastPlays[index]=='D' && pastPlays[index+1]=='2'){
-                    currentView->location[i][TRAIL_SIZE-1-turn] = DOUBLE_BACK_2;
-                } else if(pastPlays[index]=='D' && pastPlays[index+1]=='3'){
-                    currentView->location[i][TRAIL_SIZE-1-turn] = DOUBLE_BACK_3;
-                } else if(pastPlays[index]=='D' && pastPlays[index+1]=='4'){
-                    currentView->location[i][TRAIL_SIZE-1-turn] = DOUBLE_BACK_4;
-                } else if(pastPlays[index]=='D' && pastPlays[index+1]=='5'){
-                    currentView->location[i][TRAIL_SIZE-1-turn] = DOUBLE_BACK_5;
-                } else if(pastPlays[index]=='T' && pastPlays[index+1]=='P'){
-                    currentView->location[i][TRAIL_SIZE-1-turn] = TELEPORT;
-                } else {
-                    currentView->location[i][TRAIL_SIZE-1-turn]=abbrevToID(&pastPlays[index]);
-                }
-                turn++;
-                index+=40;
-            }
-            i++;
-            index = ((currentView->currRound)-6)*8 + 1 + 8*i;
-        }
-    } else {
-        int index = 1;
-        PlayerID i = 0;
+		printf("in");
+		while(i<currentView->currPlayer){
+        	int turn = 0;
+        	index-=SAME_PLACE_NEXT_TURN*NUM_PLAYERS;
+			while(turn<TRAIL_SIZE){
+				if(pastPlays[index]=='C' && pastPlays[index+1]=='?'){
+					currentView->location[i][TRAIL_SIZE-1-turn] = CITY_UNKNOWN;
+				} else if(pastPlays[index]=='S' && pastPlays[index+1]=='?'){
+					currentView->location[i][TRAIL_SIZE-1-turn] = SEA_UNKNOWN;
+				} else if(pastPlays[index]=='H' && pastPlays[index+1]=='I'){
+					currentView->location[i][TRAIL_SIZE-1-turn] = HIDE;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='1'){
+					currentView->location[i][TRAIL_SIZE-1-turn] = DOUBLE_BACK_1;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='2'){
+					currentView->location[i][TRAIL_SIZE-1-turn] = DOUBLE_BACK_2;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='3'){
+					currentView->location[i][TRAIL_SIZE-1-turn] = DOUBLE_BACK_3;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='4'){
+					currentView->location[i][TRAIL_SIZE-1-turn] = DOUBLE_BACK_4;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='5'){
+					currentView->location[i][TRAIL_SIZE-1-turn] = DOUBLE_BACK_5;
+				} else if(pastPlays[index]=='T' && pastPlays[index+1]=='P'){
+					currentView->location[i][TRAIL_SIZE-1-turn] = TELEPORT;
+				} else {
+					currentView->location[i][TRAIL_SIZE-1-turn]=abbrevToID(&pastPlays[index]);
+				}
+				turn++;
+				index+=SAME_PLACE_NEXT_TURN*NUM_PLAYERS;
+			}
+			i++;
+			index = ((currentView->currRound)-6)*SAME_PLACE_NEXT_TURN*NUM_PLAYERS + 1 + 8*i;
+		}
+		while(i<NUM_PLAYERS){
+        	int turn = 0;
+			while(turn<TRAIL_SIZE){
+				if(pastPlays[index]=='C' && pastPlays[index+1]=='?'){
+					currentView->location[i][TRAIL_SIZE-1-turn] = CITY_UNKNOWN;
+				} else if(pastPlays[index]=='S' && pastPlays[index+1]=='?'){
+					currentView->location[i][TRAIL_SIZE-1-turn] = SEA_UNKNOWN;
+				} else if(pastPlays[index]=='H' && pastPlays[index+1]=='I'){
+					currentView->location[i][TRAIL_SIZE-1-turn] = HIDE;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='1'){
+					currentView->location[i][TRAIL_SIZE-1-turn] = DOUBLE_BACK_1;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='2'){
+					currentView->location[i][TRAIL_SIZE-1-turn] = DOUBLE_BACK_2;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='3'){
+					currentView->location[i][TRAIL_SIZE-1-turn] = DOUBLE_BACK_3;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='4'){
+					currentView->location[i][TRAIL_SIZE-1-turn] = DOUBLE_BACK_4;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='5'){
+					currentView->location[i][TRAIL_SIZE-1-turn] = DOUBLE_BACK_5;
+				} else if(pastPlays[index]=='T' && pastPlays[index+1]=='P'){
+					currentView->location[i][TRAIL_SIZE-1-turn] = TELEPORT;
+				} else {
+					currentView->location[i][TRAIL_SIZE-1-turn]=abbrevToID(&pastPlays[index]);
+				}
+				turn++;
+				index+=SAME_PLACE_NEXT_TURN*NUM_PLAYERS;
+			}
+			i++;
+			index = ((currentView->currRound)-6)*SAME_PLACE_NEXT_TURN*NUM_PLAYERS + 1 + 8*i;
+		}
+	} else {
+		printf("out\n");
+		int index = 1;
+		PlayerID i = 0;
         while(i<currentView->currPlayer){
             int turn = 0;
-            while(turn<((currentView->currRound)+1)){
-                if(pastPlays[index]=='C' && pastPlays[index+1]=='?'){
-                    currentView->location[i][currentView->currRound-turn-1] = CITY_UNKNOWN;
-                } else if(pastPlays[index]=='S' && pastPlays[index+1]=='?'){
-                    currentView->location[i][currentView->currRound-turn-1] = SEA_UNKNOWN;
-                } else if(pastPlays[index]=='H' && pastPlays[index+1]=='I'){
-                    currentView->location[i][currentView->currRound-turn-1] = HIDE;
-                } else if(pastPlays[index]=='D' && pastPlays[index+1]=='1'){
-                    currentView->location[i][currentView->currRound-turn-1] = DOUBLE_BACK_1;
-                } else if(pastPlays[index]=='D' && pastPlays[index+1]=='2'){
-                    currentView->location[i][currentView->currRound-turn-1] = DOUBLE_BACK_2;
-                } else if(pastPlays[index]=='D' && pastPlays[index+1]=='3'){
-                    currentView->location[i][currentView->currRound-turn-1] = DOUBLE_BACK_3;
-                } else if(pastPlays[index]=='D' && pastPlays[index+1]=='4'){
-                    currentView->location[i][currentView->currRound-turn-1] = DOUBLE_BACK_4;
-                } else if(pastPlays[index]=='D' && pastPlays[index+1]=='5'){
-                    currentView->location[i][currentView->currRound-turn-1] = DOUBLE_BACK_5;
-                } else if(pastPlays[index]=='T' && pastPlays[index+1]=='P'){
-                    currentView->location[i][currentView->currRound-turn-1] = TELEPORT;
-                } else {
-                    currentView->location[i][currentView->currRound-turn-1]=abbrevToID(&pastPlays[index]);
-                }
+			while(turn<((currentView->currRound)+1)){
+				if(pastPlays[index]=='C' && pastPlays[index+1]=='?'){
+					currentView->location[i][currentView->currRound-turn] = CITY_UNKNOWN;
+				} else if(pastPlays[index]=='S' && pastPlays[index+1]=='?'){
+					currentView->location[i][currentView->currRound-turn] = SEA_UNKNOWN;
+				} else if(pastPlays[index]=='H' && pastPlays[index+1]=='I'){
+					currentView->location[i][currentView->currRound-turn] = HIDE;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='1'){
+					currentView->location[i][currentView->currRound-turn] = DOUBLE_BACK_1;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='2'){
+					currentView->location[i][currentView->currRound-turn] = DOUBLE_BACK_2;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='3'){
+					currentView->location[i][currentView->currRound-turn] = DOUBLE_BACK_3;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='4'){
+					currentView->location[i][currentView->currRound-turn] = DOUBLE_BACK_4;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='5'){
+					currentView->location[i][currentView->currRound-turn] = DOUBLE_BACK_5;
+				} else if(pastPlays[index]=='T' && pastPlays[index+1]=='P'){
+					currentView->location[i][currentView->currRound-turn] = TELEPORT;
+				} else {
+					currentView->location[i][currentView->currRound-turn]=abbrevToID(&pastPlays[index]);
+				}
                 turn++;
-                index+=40;
+                index+=SAME_PLACE_NEXT_TURN*NUM_PLAYERS;
             }
             i++;
-            index = 1 + 8*i;
+            index = 1 + SAME_PLACE_NEXT_TURN*i;
         }
         while(i<NUM_PLAYERS){
             int turn = 0;
-            while(turn<(currentView->currRound)){
-                if(pastPlays[index]=='C' && pastPlays[index+1]=='?'){
-                    currentView->location[i][currentView->currRound-turn-1] = CITY_UNKNOWN;
-                } else if(pastPlays[index]=='S' && pastPlays[index+1]=='?'){
-                    currentView->location[i][currentView->currRound-turn-1] = SEA_UNKNOWN;
-                } else if(pastPlays[index]=='H' && pastPlays[index+1]=='I'){
-                    currentView->location[i][currentView->currRound-turn-1] = HIDE;
-                } else if(pastPlays[index]=='D' && pastPlays[index+1]=='1'){
-                    currentView->location[i][currentView->currRound-turn-1] = DOUBLE_BACK_1;
-                } else if(pastPlays[index]=='D' && pastPlays[index+1]=='2'){
-                    currentView->location[i][currentView->currRound-turn-1] = DOUBLE_BACK_2;
-                } else if(pastPlays[index]=='D' && pastPlays[index+1]=='3'){
-                    currentView->location[i][currentView->currRound-turn-1] = DOUBLE_BACK_3;
-                } else if(pastPlays[index]=='D' && pastPlays[index+1]=='4'){
-                    currentView->location[i][currentView->currRound-turn-1] = DOUBLE_BACK_4;
-                } else if(pastPlays[index]=='D' && pastPlays[index+1]=='5'){
-                    currentView->location[i][currentView->currRound-turn-1] = DOUBLE_BACK_5;
-                } else if(pastPlays[index]=='T' && pastPlays[index+1]=='P'){
-                    currentView->location[i][currentView->currRound-turn-1] = TELEPORT;
-                } else {
-                    currentView->location[i][currentView->currRound-turn-1]=abbrevToID(&pastPlays[index]);
-                }
-                turn++;
-                index+=40;
+			while(turn<(currentView->currRound)){
+				if(pastPlays[index]=='C' && pastPlays[index+1]=='?'){
+					currentView->location[i][currentView->currRound-turn-1] = CITY_UNKNOWN;
+				} else if(pastPlays[index]=='S' && pastPlays[index+1]=='?'){
+					currentView->location[i][currentView->currRound-turn-1] = SEA_UNKNOWN;
+				} else if(pastPlays[index]=='H' && pastPlays[index+1]=='I'){
+					currentView->location[i][currentView->currRound-turn-1] = HIDE;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='1'){
+					currentView->location[i][currentView->currRound-turn-1] = DOUBLE_BACK_1;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='2'){
+					currentView->location[i][currentView->currRound-turn-1] = DOUBLE_BACK_2;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='3'){
+					currentView->location[i][currentView->currRound-turn-1] = DOUBLE_BACK_3;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='4'){
+					currentView->location[i][currentView->currRound-turn-1] = DOUBLE_BACK_4;
+				} else if(pastPlays[index]=='D' && pastPlays[index+1]=='5'){
+					currentView->location[i][currentView->currRound-turn-1] = DOUBLE_BACK_5;
+				} else if(pastPlays[index]=='T' && pastPlays[index+1]=='P'){
+					currentView->location[i][currentView->currRound-turn-1] = TELEPORT;
+				} else {
+					currentView->location[i][currentView->currRound-turn-1]=abbrevToID(&pastPlays[index]);
+				}
+				turn++;
+				index+=SAME_PLACE_NEXT_TURN*NUM_PLAYERS;
             }
             i++;
-            index = 1 + 8*i;
+            index = 1 + SAME_PLACE_NEXT_TURN*i;
         }
     }
 }
